03b88b8a73bea18de0f605fec2c792a6ebf09a83
Neelapu Venkata Naidu <114356828+venkatneelapu@users.noreply.github.com>
2024-04-25 11:48:27 +0530

    Create uppercsaeandLowercase.java

---
+++ uppercsaeandLowercase.java
@@ -0,0 +1,13 @@
+public class CaseConversion {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        
+        // Convert to upper case
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Upper case: " + upperCaseStr);
+        
+        // Convert to lower case
+        String lowerCaseStr = str.toLowerCase();
+        System.out.println("Lower case: " + lowerCaseStr);
+    }
+}
