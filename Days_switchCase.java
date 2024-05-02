b7cbe0cf68f3d1d1f55b0fd1fb7749e7d83c6d77
Neelapu Venkata Naidu <114356828+venkatneelapu@users.noreply.github.com>
2024-05-02 23:28:55 +0530

    Create Days_switchCase.java

---
+++ Days_switchCase.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Days {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int dayNum;
+    System.out.print("Enter a day number (1-7): ");
+    dayNum = in.nextInt();
+    switch(dayNum) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        System.out.println("TGIF!");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;       
+      default:
+        System.out.println("Invalid day number.");
+        break;                      
+    } 
+  }
+}
